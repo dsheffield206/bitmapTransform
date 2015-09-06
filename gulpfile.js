a23a156a19b2251d949675455e430c1ed8b66564
Tomo Sindberg <tsindberg@gmail.com>
2015-09-05 20:22:06 -0700

    updated gulpfile buffer file,current files run using gulp at terminal

--- gulpfile.js
+++ gulpfile.js
@@ -7,21 +7,32 @@ var chai = require('chai');
 var bufData = require('./lib/buffer');
 
 gulp.task('buff', function(){
-  return gulp.src('./lib/buffer.js')
-  // .pipe(jshint)
-  .pipe(gulp.dest('lib'));
+  return gulp.src('./lib/buffer.js');
+  // .pipe(gulp.dest('lib'));
 });
 
+gulp.task('trans',['oldColor'], function(){
+  return gulp.src('./lib/transform.js');
+  // .pipe(jshint);
+  // console.log('running trans from gulp');
+});
 
-// gulp.task('jshint', function() {
-//   return gulp.src(['buffer.js', 'test/**/*.js', 'lib/**/*.js', 'bin/xxx', 'gulpfile.js'])
-//     .pipe(jshint())
-//     .pipe(jshint.reporter('default'));
-// });
+gulp.task('oldColor',['buff'], function(){
+  return gulp.src('./lib/color-maps.js');
+  // .pipe(gulp.dest('lib'));
+});
+
+gulp.task('default', ['buff','oldColor']);
+
+gulp.task('jshint', function() {
+  return gulp.src(['buffer.js', 'test/**/*.js', 'lib/**/*.js', 'gulpfile.js'])
+    .pipe(jshint())
+    .pipe(jshint.reporter('default'));
+});
 
 // gulp.task('test', function() {
 //   return gulp.src('test/**/*.js')
 //     .pipe(gulpMocha({reporter: 'nyan'}));
 // });
 
-// gulp.task('default', ['jshint', 'test']);
+
