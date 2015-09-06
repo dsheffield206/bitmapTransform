26ebf00f5a3e84699debe967d231451c4ab93b9e
David Sheffield <david.sheffield21@gmail.com>
2015-09-06 13:15:16 -0700

    adding 3 tests to bmp_test

--- test/bmp_test.js
+++ test/bmp_test.js
@@ -1,25 +1,43 @@
 'use strict';
-var cp = require('../lib/buffer');
 
-console.log(cp);
 
-var expect = require('chai').expect;
 var fs = require('fs');
+var expect = require('chai').expect;
+var buff = require('../lib/buffer');
+var newBuff = require('../lib/newBuffer');
+var CPalette = require('../lib/color-map');
+var Trans = require('../lib/transform');
 
-// describe('an asynch test', function() {
-//   it('should test fs', function(done) {
-//     fs.readFile('./lib/non-palette-bitmap.bmp', function() {
-//       expect(err).to.eql(null);
-//       expect(data).to.eql('utf8');
-//       debugger;
-//       done();
-//     });
-//   });
-// });
 
-// describe('another test', function() {
-//   it('should do another test', function() {
-//     expect(err).to.eql(null);
-//     console.log('another test');
-//   });
-// });
+
+// Testing that the paletteInfo Object exists
+describe('color palette test', function() {
+  before(function (done) {
+    fs.readFile(__dirname + '/../img/palette-bitmap.bmp', function(err, data) {
+        if (err) throw err;
+        var paletteInfo = CPalette.colorPalette(paletteInfo, data);
+        debugger;
+        done();
+        console.log('running color palette test');
+    });
+  });
+  it('should read the buffer data into an Object', function() {
+      expect(paletteInfo).to.be.a(Object);
+  });
+});
+
+// Testing that the object has a width of 100
+describe('width test', function() {
+  it('should have a width of 100', function() {
+    expect(paletteInfo.width).to.eql(100);
+    console.log('width test');
+  });
+});
+
+// Testing that the object has changed after transform
+describe('transform test', function() {
+  it('should transform the colorPalette', function() {
+    expect(Trans.Transform(paletteInfo)).to.not.eql(CPalette.colorPalette(paletteInfo, data));
+    console.log('transform test');
+  });
+});
