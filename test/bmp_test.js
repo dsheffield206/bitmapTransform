3255a8e167edb53449effcfc24b97ac1e65987be
David Sheffield <david.sheffield21@gmail.com>
2015-09-07 19:42:45 -0700

    updating our bmp_test.js file

--- test/bmp_test.js
+++ test/bmp_test.js
@@ -4,39 +4,23 @@
 var fs = require('fs');
 var expect = require('chai').expect;
 var buff = require('../lib/buffer');
-var newBuff = require('../lib/newBuffer');
 var CPalette = require('../lib/color-map');
-var Trans = require('../lib/transform');
 
 
-// Testing that the paletteInfo Object exists
-describe('color palette test', function() {
-  before(function (done) {
-    fs.readFile(__dirname + '/../img/palette-bitmap.bmp', function(err, data) {
-        if (err) throw err;
-        var paletteInfo = CPalette.colorPalette(paletteInfo, data);
-        debugger;
-        done();
-        console.log('running color palette test');
-    });
-  });
-  it('should read the buffer data into an Object', function() {
-      expect(paletteInfo).to.be.a(Object);
+// Testing that CPalette data exists and is not void
+describe('object data exists test', function() {
+  it('data from the stream should not be null', function() {
+    expect(CPalette[20]).to.not.eql(null);
+    console.log('data exists test');
   });
 });
 
-// Testing that the object has a width of 100
-describe('width test', function() {
-  it('should have a width of 100', function() {
-    expect(paletteInfo.width).to.eql(100);
-    console.log('width test');
+// Testing that the data has changed
+describe('color change test', function() {
+  it('should change the data from CPalette', function() {
+    expect(CPalette.darken(data)).to.not.eql(CPalette.invert(data));
+    console.log('color change test');
   });
 });
 
-// Testing that the object has changed after transform
-describe('transform test', function() {
-  it('should transform the colorPalette', function() {
-    expect(Trans.Transform(paletteInfo)).to.not.eql(CPalette.colorPalette(paletteInfo, data));
-    console.log('transform test');
-  });
-});
+exports.test = test;
